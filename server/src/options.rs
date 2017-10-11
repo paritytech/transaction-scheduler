2cb201dae3fd7b0806e87f983b751098f76ee7c0
Tomasz Drwięga <tomusdrw@gmail.com>
2017-10-11 09:23:51 +0200

    Disable min delay.

--- server/src/options.rs
+++ server/src/options.rs
@@ -7,12 +7,8 @@ pub struct Options {
     pub max_gas: u64,
     /// Minimal gas price
     pub min_gas_price: u64,
-    /// Min number of blocks in future to schedule for.
-    pub min_schedule_block: u64,
     /// Max number of blocks in future to schedule for.
     pub max_schedule_block: u64,
-    /// Min number of seconds in future to schedule for.
-    pub min_schedule_seconds: u64,
     /// Max number of seconds in future to schedule for.
     pub max_schedule_seconds: u64,
     /// Require nonce to be equal to current nonce (and not only greater).
