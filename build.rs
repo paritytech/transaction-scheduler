af74c6109361c504711583bff39402775a2f1689
Tomasz Drwięga <tomusdrw@gmail.com>
2017-10-26 00:45:04 +0200

    Precompiled.

---
+++ build.rs
@@ -0,0 +1,5 @@
+extern crate parity_dapps_glue;
+
+fn main() {
+	parity_dapps_glue::generate();
+}
---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+#[cfg(feature = "with-syntex")]
+include!(concat!(env!("OUT_DIR"), "/lib.rs"));
+
+#[cfg(not(feature = "with-syntex"))]
+include!("lib.rs.in");
